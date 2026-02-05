edd9d885ce65ef9e900ad10b793702ceffbfc53f
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2026-02-05 16:21:20 +0900

    upd

--- openssl/openssl.bat
+++ openssl/openssl.bat
@@ -4,8 +4,8 @@ setlocal
 
 REM Docker build
 docker build . -t openssl111w-mingw:local ^
-  --build-arg OPENSSL_TAG=OpenSSL_1_1_1w ^
-  --build-arg OPENSSL_DIR=openssl-1.1.1w
+  --build-arg OPENSSL_TAG=openssl-3.0.19 ^
+  --build-arg OPENSSL_DIR=openssl-3.0.19
 
 REM Create container and capture container ID
 for /f %%i in ('docker create openssl111w-mingw:local') do set CID=%%i
